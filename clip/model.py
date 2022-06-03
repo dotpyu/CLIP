36fd0bec5415b0b5778d858335be6eec0f4f25c9
Peilin <peilin_yu@brown.edu>
2022-06-03 10:41:15 -0400

    debugging

--- clip/model.py
+++ clip/model.py
@@ -160,7 +160,8 @@ class LayerNorm(nn.LayerNorm):
 
     def forward(self, x: torch.Tensor):
         orig_type = x.dtype
-        ret = super().forward(x.type(torch.float32))
+        x = x.float()
+        ret = super().forward(x)
         return ret.type(orig_type)
 
 
